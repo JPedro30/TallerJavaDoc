ebee7eaa02b4be2662540b026f76baa20ec7a93d
JPedro-ESP <jpgonzalezhens@gmail.com>
2026-01-22 20:05:58 +0100

    22012026

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -1,6 +1,6 @@
 package vista;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prueba de que git funciona");
+        System.out.println("Prueba de que visual hace pull & request");
     }
 }
